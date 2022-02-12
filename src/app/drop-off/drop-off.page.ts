f40e38e35f90cd3da5c43e69f84f903fc53f58e2
Jose leon <jose_l_leon@gmail.com>
2022-02-12 13:02:44 -0500

    test

--- src/app/drop-off/drop-off.page.ts
+++ src/app/drop-off/drop-off.page.ts
@@ -13,15 +13,18 @@ export class DropOffPage implements OnInit {
 
   @ViewChild('filePicker', { static: false }) filePickerRef: ElementRef<HTMLInputElement>;
   photo: SafeResourceUrl;
+  photoO: SafeResourceUrl;
   isDesktop: boolean;
   stillValidating: boolean = true;
+  currentMiles: string;
 
 
   constructor(
     private platform: Platform,
     private zone: NgZone,
-    private sanitizer: DomSanitizer) { }
-
+    private sanitizer: DomSanitizer) { 
+      this.currentMiles = '152900';
+    }
 
   ngOnInit() {
     if ((this.platform.is('mobile') && this.platform.is('hybrid')) || this.platform.is('desktop')) {
@@ -51,6 +54,24 @@ export class DropOffPage implements OnInit {
     this.photo = this.sanitizer.bypassSecurityTrustResourceUrl(image && (image.dataUrl));
   }
 
+
+  async getPictureO() {
+    if (!Capacitor.isPluginAvailable('Camera') || (this.isDesktop )) {
+      this.filePickerRef.nativeElement.click();
+      return;
+    }
+
+    const imageO = await Camera.getPhoto({
+      quality: 100,
+      width: 400,
+      allowEditing: false,
+      resultType: CameraResultType.DataUrl,
+      source: CameraSource.Prompt
+    });
+
+    this.photoO = this.sanitizer.bypassSecurityTrustResourceUrl(imageO && (imageO.dataUrl));
+  }
+
   onFileChoose(event: Event) {
     const file = (event.target as HTMLInputElement).files[0];
     const pattern = /image-*/;
@@ -63,6 +84,23 @@ export class DropOffPage implements OnInit {
 
     reader.onload = () => {
       this.photo = reader.result.toString();
+    };
+    reader.readAsDataURL(file);
+
+  }
+
+  onFileChooseO(event: Event) {
+    const file = (event.target as HTMLInputElement).files[0];
+    const pattern = /image-*/;
+    const reader = new FileReader();
+
+    if (!file.type.match(pattern)) {
+      console.log('File format not supported');
+      return;
+    }
+
+    reader.onload = () => {
+      this.photoO = reader.result.toString();
     };
     reader.readAsDataURL(file);
 
